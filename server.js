5bc7a715f2e38f6125c8cb8786ae93aab8f645f2
Morgan Styers <morganstyers@gmail.com>
2019-12-07 00:18:11 -0500

    scraping connection

--- server.js
+++ server.js
@@ -1,98 +1,54 @@
 var cheerio = require("cheerio");
 var axios = require("axios");
+var express = require("express");
+var mongojs = require("mongojs");
+
+var app = express();
+
+var databaseUrl = "musicnews";
+var collections = ["reviews"];
+
+var db = mongojs(databaseUrl, collections);
+
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+app.get("/scraped", function(req,res){
+    res.send("testing")});
+    app.get("/all", function(req,res){
+        db.reviews.find({}, function (err,found) {
+            if (err){
+                console.log(err)
+            }
+            else{
+                res.json(found)
+            }
+        })
+        });
+
 
 axios.get("https://www.pitchfork.com").then(function(response) {
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
   var $ = cheerio.load(response.data);
 
-  // An empty array to save the data that we'll scrape
   var results = [];
 
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
   $(".album-details").each(function(i, element) {
 
     var title = $(element).find("h2").text();
     var text= $(element).f
     var link = $(element).find("a").attr("href");
 
-    // Save these results in an object that we'll push into the results array we defined earlier
     results.push({
       title: title,
       link: link
     });
   });
 
-  // Log the results once you've looped through each of the elements found with cheerio
   console.log(results);
 });
 
 
-/*
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-    });
-
-    res.send("Scrape Complete");
-
-  });
-});
-
-app.get("/articles", function(req, res) {
-  db.Article.find({})
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});*/
+app.listen(7000, function() {
+    console.log("App running on port " + 7000 + "!");
+  })
