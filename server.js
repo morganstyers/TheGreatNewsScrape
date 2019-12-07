c3b4b2b4e461e7de7072118a8a2df1027f528e2d
Morgan Styers <morganstyers@gmail.com>
2019-12-07 00:29:09 -0500

    appending to route

--- server.js
+++ server.js
@@ -2,53 +2,50 @@ var cheerio = require("cheerio");
 var axios = require("axios");
 var express = require("express");
 var mongojs = require("mongojs");
+var logger = require('morgan')
 
 var app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 
 var databaseUrl = "musicnews";
 var collections = ["reviews"];
 
 var db = mongojs(databaseUrl, collections);
 
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+db.on("error", function (error) {
+    console.log("Database Error:", error);
 });
-app.get("/scraped", function(req,res){
-    res.send("testing")});
-    app.get("/all", function(req,res){
-        db.reviews.find({}, function (err,found) {
-            if (err){
-                console.log(err)
-            }
-            else{
-                res.json(found)
-            }
-        })
+app.get("/all", function (req, res) {
+    axios.get("https://www.pitchfork.com").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        var results = [];
+
+        $(".album-details").each(function (i, element) {
+
+            var title = $(element).find("h2").text();
+            var text = $(element).find('p').text();
+            var link = $(element).find("a").attr("href");
+
+            results.push({
+                title: title,
+                text:text,
+                link: link
+            });
         });
 
-
-axios.get("https://www.pitchfork.com").then(function(response) {
-
-  var $ = cheerio.load(response.data);
-
-  var results = [];
-
-  $(".album-details").each(function(i, element) {
-
-    var title = $(element).find("h2").text();
-    var text= $(element).f
-    var link = $(element).find("a").attr("href");
-
-    results.push({
-      title: title,
-      link: link
+        console.log(results);
+        res.send(results)
     });
-  });
-
-  console.log(results);
 });
 
 
-app.listen(7000, function() {
+app.listen(7000, function () {
     console.log("App running on port " + 7000 + "!");
-  })
+})
